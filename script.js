0bef9c59f8d314f99cbcfebaaabf25c5c88201ac
pixel-coder | Estif <legendestif@gmail.com>
2025-08-22 13:11:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+<!-- Google Tag Manager -->
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TVCHCNT3');
+<!-- End Google Tag Manager -->
+
 // Google Analytics initialization
 window.dataLayer = window.dataLayer || [];
 function gtag(){ dataLayer.push(arguments); }
@@ -22,5 +30,6 @@ const menuToggle = document.querySelector(".menu-toggle");
     root.setAttribute('data-theme', isDark ? 'light' : 'dark');
     localStorage.setItem('theme', isDark ? 'light' : 'dark');
   });
+
 
 
